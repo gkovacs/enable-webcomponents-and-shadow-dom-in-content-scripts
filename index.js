0cb6e5cda0f9c5246ecf98322cb13fb4603d0c88
Geza Kovacs <gkovacs@stanford.edu>
2016-06-30 14:16:30 -0700

    fix multiple inclusion error

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-if not window.enable_webcomponents_in_content_scripts
-  window.enable_webcomponents_in_content_scripts = true
+if (!window.enable_webcomponents_in_content_scripts) {
+  window.enable_webcomponents_in_content_scripts = true;
   document.registerElement = null;
   require('webcomponentsjs-custom-element-v1');
+}
