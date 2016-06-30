3137b61f466942a7f20da2c3ee11dca9a7be590d
Geza Kovacs <gkovacs@stanford.edu>
2016-06-30 14:14:20 -0700

    include only once

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-document.registerElement = null;
-require('webcomponentsjs-custom-element-v1');
+if not window.enable_webcomponents_in_content_scripts
+  window.enable_webcomponents_in_content_scripts = true
+  document.registerElement = null;
+  require('webcomponentsjs-custom-element-v1');
