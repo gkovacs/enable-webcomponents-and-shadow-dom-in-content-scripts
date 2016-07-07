8c3cc18785a57950d18d3b840a189689bb1754e9
Geza Kovacs <gkovacs@stanford.edu>
2016-07-07 02:57:09 -0700

    remove polyfilling of customelements v1, only polyfill customelements v0

--- index.js
+++ index.js
@@ -4,10 +4,3 @@ if (document.registerElement && /\{\s*\[native code\]\s*\}/.test(document.regist
 if (document.registerElement == null) {
   require('webcomponentsjs-custom-element-v0');
 }
-
-if (window.customElements && /\{\s*\[native code\]\s*\}/.test(window.customElements)) {
-  window.customElements = null; // disable native implementation of v0
-}
-if (window.customElements == null) {
-  require('webcomponentsjs-custom-element-v1');
-}
