b38ae789245564dfb509deb58aef1ed64e487ae3
Geza Kovacs <geza0kovacs@gmail.com>
2018-07-06 17:51:05 -0700

    added shadow dom

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ if (document.registerElement && /\{\s*\[native code\]\s*\}/.test(document.regist
   document.registerElement = null; // disable native implementation of v0
 }
 if (document.registerElement == null) {
+  require('webcomponentsjs-shadow-dom-v0');
   require('webcomponentsjs-custom-element-v0');
 }
